1abe980d2f5d56c933c91c76d7fa261e83ca5370
mochibot <amusaur@gmail.com>
2019-08-06 18:18:48 -0700

    Rearranged components and implemented react router to separate blogpage and postpage

--- blog-app/src/components/Post.js
+++ blog-app/src/components/Post.js
@@ -1,42 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Card, Button, List } from 'antd';
-import Comment from './Comment';
-import CommentForm from './CommentForm';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Button } from 'antd';
 
 const Post = (props) => {
-  const [comments, setComments] = useState([]);
-  
-  let baseURL = 'http://localhost:8000/api/posts';
-
-  useEffect(() => {
-    axios.get(`${baseURL}/${props.post.id}/comments`)
-      .then(response => {
-        console.log('fetching comments success: ', response);
-        setComments(response.data)
-      })
-      .catch(error => {
-        console.log('fetching comments error: ', error);
-      })
-  }, []);
-
-  const addComment = (comment) => {
-    axios.post(`${baseURL}/${props.post.id}/comments`, comment)
-      .then(response => {
-        console.log('adding comments success: ', response);
-        return axios.get(`${baseURL}/${props.post.id}/comments`)
-          .then(response => {
-            console.log('fetching comments success: ', response);
-            setComments(response.data)
-          })
-          .catch(error => {
-            console.log('fetching comments error: ', error);
-          })
-      })
-      .catch(error => {
-        console.log('fetching comments error: ', error);
-      })
-  }
 
   return (
     <Card title={props.post.title} style={{width: '80%', margin: '20px auto'}}>
@@ -44,12 +10,7 @@ const Post = (props) => {
       <div>Created on: {props.post['created_at']}</div>
       <Button onClick={(event) => props.selectPost(event, props.post)}>Edit</Button>
       <Button onClick={(event) => props.deletePost(event, props.post.id)}>Delete</Button>
-      <Card type='inner'>
-        <CommentForm addComment={addComment}/>
-        <List>
-          {comments.map(item => <Comment key={`comment ${item.id}`} comment={item}/>)}
-        </List>
-      </Card>
+      <Link to={`/posts/${props.post.id}`}>View comments</Link>
     </Card>
   )
 }
---
+++ blog-app/src/pages/BlogPage.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import PostList from '../components/PostList';
+import PostForm from '../components/PostForm';
+
+
+const BlogPage = () => {
+  const [posts, setPost] = useState([]);
+  const [activePost, setActivePost] = useState(null);
+
+  let baseURL = 'http://localhost:8000/api/posts';
+
+  useEffect(() => {
+    axios.get(baseURL)
+      .then(response => {
+        console.log('fetching posts success: ', response);
+        setPost(response.data.sort((a, b) => b.id - a.id));
+      })
+      .catch(error => {
+        console.log('fetching posts error: ', error);
+      })
+  }, [baseURL]);
+  
+  const addPost = (post) => {
+    axios.post(baseURL, post)
+      .then(response => {
+        return axios.get(baseURL)
+          .then(response => {
+            console.log('fetching posts success: ', response);
+            setPost(response.data.sort((a, b) => b.id - a.id));
+          })
+          .catch(error => {
+            console.log('fetching posts error: ', error);
+          })
+      })
+      .catch(error => {
+        console.log('fetching posts error: ', error);
+      })
+  }
+
+  const deletePost = (event, postId) => {
+    event.preventDefault();
+    axios.delete(baseURL + `/${postId}`)
+      .then(response => {
+        console.log('delete post success: ', response);
+        return axios.get(baseURL)
+          .then(response => {
+            console.log('fetching posts success: ', response);
+            setPost(response.data.sort((a, b) => b.id - a.id));
+          })
+          .catch(error => {
+            console.log('fetching posts error: ', error);
+          })
+      })
+      .catch(error => {
+        console.log('delete post error: ', error);
+      })
+  }
+
+  const editPost = (id, post) => {
+    axios.put(baseURL + `/${id}`, post)
+      .then(response => {
+        console.log('edit post success: ', response);
+        return axios.get(baseURL)
+          .then(response => {
+            console.log('fetching posts success: ', response);
+            setPost(response.data.sort((a, b) => b.id - a.id));
+            setActivePost(null)
+          })
+          .catch(error => {
+            console.log('fetching posts error: ', error);
+          })
+      })
+      .catch(error => {
+        console.log('edit post error: ', error);
+      })
+  }
+
+  const selectPost = (event, post) => {
+    event.preventDefault();
+    setActivePost(post)
+  }
+
+  return (
+    <div>
+      <PostForm addPost={addPost} editPost={editPost} activePost={activePost} />
+      <PostList posts={posts} deletePost={deletePost} selectPost={selectPost}/>
+    </div>
+  );
+}
+
+export default BlogPage;
